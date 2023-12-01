ef01e431b633375ae1bbf36497d93562b94d68fe
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-01 10:39:53 -0700

    added content to Header.svelte

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,24 @@
+<header>
+  <div>
+    THE C.S.C CHRONICLES
+  </div>
+</header>
+
+<style>
+ header {
+    /* margin: auto; */
+   
+    text-align: center;
+
+    font-family: fantasy;
+    font-size: 4rem;
+    font-weight: bolder;
+    
+    /* padding: 1rem; */
+
+    /* background-color: azure; */
+    color: darkcyan;
+
+    border-bottom: 10px solid;
+  }
+</style>
