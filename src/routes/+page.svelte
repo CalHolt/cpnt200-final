31ad98129c92956271a65fe294cfce9b35a79b41
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-01 11:48:42 -0700

    added hero-section image for blog site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,8 +6,43 @@
 <Header />
 <slot />
 
+<!--Hero Section Image-->
+<body>
+  <span>
+    <h3>BLOG</h3>
+  </span>
+</body>
+
 <ul>
   {#each data.blog as post}
    <h1>{post.blog_title}</h1> 
   {/each}
 </ul>
+
+<style>
+  body {
+    background-image: url("images/blog-hero-section-image.jpg");
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    
+    height: 80vh;
+    width: 100%;
+
+    margin: 0;
+  }
+
+  h3 {
+    z-index: -1;
+
+    font-family: fantasy;
+    font-size: 7rem;
+
+    color: white;
+
+    text-align: center;
+    margin: auto;
+
+    padding-top: 13rem;
+  }
+</style>
